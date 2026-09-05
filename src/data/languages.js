99c9c89792f0360fbd8dc8e6504bb6e2e89e5303
TheLordSouth <bhaiosi@gmail.com>
2026-09-05 22:00:00 +1000

    Add language configuration

---
+++ src/data/languages.js
@@ -0,0 +1,6 @@
+export const languages = [
+  { code: 'niu', label: 'Vagahau Niue', short: 'Niue', enabled: true },
+  { code: 'sm', label: 'Gagana Samoa', short: 'Samoa', enabled: false },
+  { code: 'to', label: 'Lea Faka-Tonga', short: 'Tonga', enabled: false },
+  { code: 'fj', label: 'Vosa Vakaviti', short: 'Fiji', enabled: false },
+]
