ebcd5a2f16325e1951e789e7e7d7f3859eaf33e9
TheLordSouth <bhaiosi@gmail.com>
2026-09-05 22:12:13 +1000

    Refine STORM visual centrepiece

--- src/components/StormOrb.jsx
+++ src/components/StormOrb.jsx
@@ -1,13 +1,17 @@
 export default function StormOrb({ state = 'idle' }) {
   return (
     <div className={`storm-orb storm-orb--${state}`} aria-label={`STORM is ${state}`}>
-      <div className="storm-orb__halo storm-orb__halo--outer" />
-      <div className="storm-orb__halo storm-orb__halo--inner" />
+      <div className="storm-orb__aura" />
+      <div className="storm-orb__ring storm-orb__ring--outer" />
+      <div className="storm-orb__ring storm-orb__ring--inner" />
       <div className="storm-orb__core">
-        <span className="storm-orb__line storm-orb__line--a" />
-        <span className="storm-orb__line storm-orb__line--b" />
-        <span className="storm-orb__line storm-orb__line--c" />
+        <div className="storm-orb__glass" />
+        <span className="storm-orb__current storm-orb__current--a" />
+        <span className="storm-orb__current storm-orb__current--b" />
+        <span className="storm-orb__current storm-orb__current--c" />
+        <span className="storm-orb__current storm-orb__current--d" />
       </div>
+      <div className="storm-orb__shadow" />
     </div>
   )
 }
