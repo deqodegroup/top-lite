296b45ab7e81a96ebad9eefa51afc5be0301ffcc
TheLordSouth <bhaiosi@gmail.com>
2026-09-16 14:19:53 +1000

    Load avatar mode styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/global.css'
 import './styles/future.css'
+import './styles/avatar.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
